7dc799487e4f6e9dbe7409e67dd6618fc744b646
shubham <shubham.chatterjee94@gmail.com>
2018-02-14 16:24:01 -0500

    Changed usage options to include PageRank and PersonalisedPageRank

--- CS980/src/main/ProjectMain.java
+++ CS980/src/main/ProjectMain.java
@@ -1,6 +1,10 @@
 package main;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
+import pageRank.PageRank;
+import pageRank.PersonalisedPageRank;
 
 public class ProjectMain 
 {
@@ -16,27 +20,48 @@ public class ProjectMain
 			new BuildIndex(indexDir,cborDir);
 			BuildIndex.createIndex();
 		}
-		else if(args[0].equalsIgnoreCase("-s") && args[1].equalsIgnoreCase("-p"))
+		else if(args[0].equalsIgnoreCase("-sp"))
 		{
 			System.out.println("Searching index for pages");
-			String indexDir = args[2];
-			String outDir = args[3];
-			String cborOutline = args[4];
-			String outFile = args[5];
-			int top = Integer.parseInt(args[6]);
+			String indexDir = args[1];
+			String outDir = args[2];
+			String cborOutline = args[3];
+			String outFile = args[4];
+			int top = Integer.parseInt(args[5]);
 			new SearchIndex(indexDir,outDir,cborOutline,outFile,top);
 			SearchIndex.searchPages();
 		}
-		else if(args[0].equalsIgnoreCase("-s") && args[1].equalsIgnoreCase("-s"))
+		else if(args[0].equalsIgnoreCase("-ss"))
 		{
 			System.out.println("Searching index for sections");
-			String indexDir = args[2];
-			String outDir = args[3];
-			String cborOutline = args[4];
-			String outFile= args[5];
-			int top = Integer.parseInt(args[6]);
+			String indexDir = args[1];
+			String outDir = args[2];
+			String cborOutline = args[3];
+			String outFile= args[4];
+			int top = Integer.parseInt(args[5]);
 			new SearchIndex(indexDir,outDir,cborOutline,outFile,top);
 			SearchIndex.searchSections();
+		}
+		else if(args[0].equalsIgnoreCase("-pr"))
+		{
+			System.out.println("Doing PageRank...");
+			String file = args[1];
+			double alpha = Double.parseDouble(args[2]);
+			PageRank p = new PageRank(file,alpha);
+			p.calculate();
+		}
+		else if(args[0].equalsIgnoreCase("-ppr"))
+		{
+			System.out.println("Doing PersonalisedPageRank...");
+			String file = args[1];
+			double alpha = Double.parseDouble(args[2]);
+			int n = Integer.parseInt(args[3]);
+			int c=4;
+			ArrayList<String> seed = new ArrayList<String>();
+			for(int i = 1 ; i <= n ; i++)
+				seed.add(args[c++]);
+			PersonalisedPageRank p = new PersonalisedPageRank(file,alpha,seed);
+			p.calculate();
 		}
 		else
 		{
@@ -46,16 +71,32 @@ public class ProjectMain
 	}
 	private static void use()
 	{
-		System.out.println("There are two options with which you can run this software. Using option -b builds"
-				+ "the index whereas using option -s searches the index. When using option -s, there are two modes "
-				+ "in which the search can be done. Use option -p after -s to search the index for page queries and"
-				+ "use option -s after -s to search the index for section queries.");
-		System.out.println("When using the -b option:");
+		System.out.println("************************************************************************************************************************************************************");
+		System.out.println("             										USAGE OPTIONS                   																			");
+		System.out.println("************************************************************************************************************************************************************");
+		System.out.println("-u: Display usage");
+		System.out.println("-b: Build Index");
+		System.out.println("-sp: Search Index for Page queries");
+		System.out.println("-ss: Search Index for Section queries");
+		System.out.println("-pr: Run PageRank Algorithm on a Graph");
+		System.out.println("-ppr: Run PersonalisedPageRank Algorithm on a Graph");
+		System.out.println();
+		System.out.println("************************************************************************************************************************************************************");
+		System.out.println("             										USAGE SYNTAX                   																			");
+		System.out.println("************************************************************************************************************************************************************");
+		
+		
+		
 		System.out.println("java -jar $jar file$ -b $path to index directory$ $path to directory containing paragrapgh cbor file$");
-		System.out.println("When using -s option:");
-		System.out.println("java -jar $jar file$ -s -p $path to index directory$ $path to output directory$"
+		
+		System.out.println("java -jar $jar file$ -sp $path to index directory$ $path to output directory$"
 				+" "+"$path to cbor outline file$ $name of paragragh run file$ $top how many results$");
-		System.out.println("java -jar $jar file$ -s -s $path to index directory$ $path to output directory$"
+		
+		System.out.println("java -jar $jar file$ -ss $path to index directory$ $path to output directory$"
 				+" "+"$path to cbor outline file$ $name of section run file$ $top how many results$");
+		
+		System.out.println("java -jar $jar file$ -pr $path to graph file$ $value of random jump (alpha)$");
+		
+		System.out.println("java -jar $jar file$ -ppr $path to graph file$ $value of random jump (alpha)$ $size of seed set$ $seed values$");
 	}
 }
