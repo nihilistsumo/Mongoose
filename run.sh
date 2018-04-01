7505a31c6966fd5fbca6608c039bf26c2dbfd257
nihilistsumo <kashyapisumanta@gmail.com>
2018-04-01 13:56:56 -0400

    fixed run.sh

--- run.sh
+++ run.sh
@@ -7,9 +7,11 @@ mvn package
 echo "Done compiling and packaging"
 #cd ..
 echo "Adding necessary configuration files and wordnet database..."
-jar uf target/Mongoose-0.0.1-SNAPSHOT-jar-with-dependencies.jar /home/mong/similarity.conf /home/mong/jawjaw.conf /home/mong/wnjpn.db
+cp /home/mong/similarity.conf /home/mong/jawjaw.conf /home/mong/wnjpn.db .
+jar uf target/Mongoose-0.0.1-SNAPSHOT-jar-with-dependencies.jar similarity.conf jawjaw.conf wnjpn.db
+rm similarity.conf jawjaw.conf wnjpn.db
 echo "Done"
 #cd Mongoose
 mkdir ../mongoose-results
 echo "Now running code..."
-java -jar target/Mongoose-0.0.1-SNAPSHOT-jar-with-dependencies.jar
+#java -jar target/Mongoose-0.0.1-SNAPSHOT-jar-with-dependencies.jar
