6482f13acb1450cfa8e0f9f35a0a2c35d183b229
sumanta <sumanta@sumanta-Aspire-A515-51G>
2018-04-19 16:21:12 -0400

    fix run script 2

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Running Mongoose"
 pagerunfilename=comb-top200-laura-cand-train
-java -jar target/Mongoose-0.0.1-SNAPSHOT-jar-with-dependencies.jar -p ../../cand-sets/$pagerunfilename -page-run mongoose-results/simpara-$pagerunfilename
+java -jar target/Mongoose-0.0.1-SNAPSHOT-jar-with-dependencies.jar -p ../../cand-sets/$pagerunfilename-page-run mongoose-results/simpara-$pagerunfilename
