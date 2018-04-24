16d95821d93f0c610524b08ff5b50caddfd6ff44
tarunprasadg92 <tarunprasadg92@gmail.com>
2018-04-23 23:07:23 -0400

    Add Candidate set generation using Entity Extraction

---
+++ src/edu/unh/cs/treccar/proj/entity/EntityExtraction.java
@@ -0,0 +1,94 @@
+package edu.unh.cs.treccar.proj.entity;
+
+import java.io.*;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ArrayList;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.*;
+
+/*
+ * @author Tarun Prasad
+ * 
+ * This class uses DBPedia Spotlight for Entity Retrieval
+ */
+public class EntityExtraction
+{
+	List<String> entities;
+	String command;
+	String paragraph_text;
+	
+	/*
+	 * Constructor to initialize the curl script and text to retrieve entities
+	 */
+	public EntityExtraction(String curl_path, String ptext)
+	{
+		command = curl_path + "curl_command.sh";
+		paragraph_text = ptext;
+		entities = new ArrayList<String>();
+	}
+	
+	/*
+	 * Helper function to print out and verify the entities
+	 */
+	public void printEntities(List<String> entities) 
+	{
+		System.out.println("Entities : ");
+		
+		for (String entity : entities) 
+		{
+			System.out.print(entity + "\t");
+		}
+	}
+	
+	/*
+	 * Function to retrieve entities from text by making a CURL request to DBPedia
+	 */
+	public List<String> retrieveEntities() throws IOException, ParseException
+	{
+		ProcessBuilder pb = new ProcessBuilder(command, paragraph_text);
+		Process proc = pb.start();
+		
+		BufferedReader output = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+		String out = null;
+		out = output.readLine();
+		
+		if (out == null)
+		{
+			System.out.println("\nNull returned..");
+			return entities;
+		}
+		
+		JSONParser parser = new JSONParser();
+		JSONObject json = new JSONObject();
+		json = (JSONObject)parser.parse(out);
+		
+		if (json.get("Resources") != null) 
+		{
+			JSONArray Resources = (JSONArray)json.get("Resources");
+			Iterator<JSONObject> uris = Resources.iterator();
+			
+			while (uris.hasNext()) 
+			{
+				JSONParser sub_parser = new JSONParser();
+				JSONObject u = (JSONObject)sub_parser.parse(uris.next().toString());
+				String entity_with_path = (String)u.get("@URI");
+				String[] entity_path_name = entity_with_path.split("/");
+				String[] sub = entity_path_name[entity_path_name.length - 1].split("_");
+				
+				String entity = "";
+			
+				for (int i = 0; i < sub.length; i++) 
+				{
+					entity += sub[i] + " ";
+				}
+				
+				entities.add(entity.trim());
+			}
+		}
+		
+		return entities;
+	}
+}
